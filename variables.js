5b5304334cfb30a3a17fa97034c7b079f572e0ee
julian <julilc324@gmail.com>
2022-03-08 14:18:21 -0500

    pendiente tipos datos operadores

---
+++ variables.js
@@ -0,0 +1,36 @@
+'use strict'
+
+//variables
+//var nombre = "julian"
+//var apellido = "londoño"
+//console.log(nombre+" "+apellido)
+
+//Prueba con var
+//var numero = 40
+//console.log(numero)
+
+//if(true){
+//    numero = 60
+//    console.log(numero)
+//}
+
+//console.log(numero)
+
+//Prueba con let
+//var numero = 40
+//console.log(numero)
+
+//if(true){
+//    let numero = 60
+//    console.log(numero)
+//}
+
+//console.log(numero)
+
+//constantes
+//const nombre = "julian"
+//nombre = "juliana"
+//console.log(nombre)
+
+
+
