9931b78102aba061dd319158875de7594b08d1df
julian <julilc324@gmail.com>
2022-03-13 11:25:34 -0500

    pendiente bucles

---
+++ condicionales.js
@@ -0,0 +1,73 @@
+'use strict'
+
+//Operadores relacionales
+/* Mayor >
+Menor <
+Mayor o igual >=
+Menir o igual <=
+igual ==
+Distinto !=*/
+
+//Operadores logicos
+/* AND = &&
+OR = ||
+NOT = ! */
+
+//Condicionales
+//if - else
+/*var a = 2
+var b = 4
+if (a > b){
+    console.log("a es mayor que b")
+}else{
+    console.log("b es mayor que a")
+}*/
+
+/*var a = true
+var b = true
+var c = false
+
+if(a == true && c == true){
+    console.log("todo verdadero");
+}else{
+    console.log("alguno falso");
+}*/
+
+/*if(a == false || c == false){
+    console.log("alguno es false");
+
+}else{
+    console.log("todo es verdadero")
+}*/
+
+//switch
+var dias = "sabado"
+
+switch(dias){
+    case "lunes":
+        console.log("lunes")
+    break
+
+    case "martes":
+        console.log("martes")
+    break
+
+    case "miercoles":
+        onsole.log("miercoles")
+    break
+
+    case "jueves":
+        console.log("Jueves")
+    break
+
+    case "viernes":
+        onsole.log("viernes")
+    break
+
+    default:
+        console.log("fin de semana")
+}
+
+
+
+
--- operadores.js
+++ operadores.js
@@ -1,7 +1,7 @@
 'use strict'
 
 //Tipo de datos
-var entero = 20
+/*var entero = 20
 var texto = "hola como estas"
 var booleano = false
 var decimal = 5.2
@@ -9,10 +9,10 @@ console.log("esto es un entero: " +entero)
 console.log("esto es un texto: " +texto)
 console.log("esto es un booleano: " +booleano)
 console.log("esto es un decimal: " +decimal)
-console.log("el tipo de dato: " +typeof entero)
+console.log("el tipo de dato: " +typeof entero)*/
 
 // Operadores
-var numero1 = 1
+/*var numero1 = 1
 var numero2 = 4
 var suma = numero1 + numero2
 var resta = numero1 - numero2
@@ -21,4 +21,4 @@ var division = numero1 / numero2
 console.log("el resultado de la suma:" +suma)
 console.log("el resultado de la resta: " +resta)
 console.log("el resultado de la multiplicacion: " +multiplicacion)
-console.log("el resultado de la division: " +division)
+console.log("el resultado de la division: " +division)*/
