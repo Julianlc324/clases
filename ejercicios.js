b2dc1e946000c6973cec05528be04afa5d7ac505
julian <julilc324@gmail.com>
2022-03-18 22:42:47 -0500

    pendiente ejercicios parte2

--- ejercicios.js
+++ ejercicios.js
@@ -20,7 +20,7 @@ if(numero1 > numero2){
 }*/
 
 //Ejercicio 2
-var contador = 0
+/*var contador = 0
 var numero = 0
 var suma = 0
 var media = 0
@@ -37,4 +37,89 @@ do {
 } while(numero >= 0);
 media = suma/contador
 console.log("suma igual: "+suma)
-console.log("promedio igual: "+media)
+console.log("promedio igual: "+media)*/
+
+//Ejercicio 3
+/*var numero1
+var numero2
+numero1 = Number(prompt("ingrese el numero1: "))
+numero2 = Number(prompt("ingrese el numero2: "))
+if(isNaN(numero1) || isNaN(numero2)){
+    console.log("error en la valores de los numeros")
+}else{
+    if(numero1 < numero2){
+        for (let i = numero1; i <= numero2 ; i++) {
+            console.log("intervalo de numero: "+i)
+        }
+    }else{
+        for (let i = numero2; i <= numero1 ; i++) {
+            console.log("intervalo de numero: "+i)
+        }
+    }
+}*/
+
+//Ejercicio 4
+/*var numero1
+var numero2
+numero1 = Number(prompt("ingrese el numero1: "))
+numero2 = Number(prompt("ingrese el numero2: "))
+if(isNaN(numero1) || isNaN(numero2)){
+    console.log("error en la valores de los numeros")
+}else{
+    if(numero1 < numero2){
+        for (let i = numero1; i <= numero2; i++){
+            let impar = i%2
+            if(impar == 1){
+                console.log("mi numero impar es: "+i)
+            }else{
+                console.log("mi numero par es: "+i)
+            }
+        }
+    }else{
+        for (let i = numero2; i <= numero1; i++){
+            let impar = i%2
+            if(impar == 1){
+                console.log("mi numero impar es: "+i)
+            }else{
+                console.log("mi numero par es: "+i)
+            }   
+        }
+    }
+}*/
+
+// Ejerciciio 5
+/*var numero
+numero = Number(prompt("ingrese un numero: "))
+if(isNaN(numero)){
+    console.log("numero invalido")
+}else{
+    var i = 0
+    while(i<=numero){
+        let par = i%2
+        if(par == 0){
+            console.log("numero par: "+i)
+        }else{
+            console.log("numero impar: "+i)
+        }
+        i++
+    }
+}*/
+
+// Ejercicio 6
+var numero 
+var resultado
+numero = Number(prompt("Ingrese un numero: "))
+while(numero < 0 || isNaN(numero)){
+    numero = Number(prompt("Ingrese un numero valido: "))
+}
+for (let i = 0; i <= numero; i++) {
+    resultado = i%2
+    if(resultado == 0){
+        console.log("es un numero par: "+i)
+    }else{
+        console.log("es un numero impar: "+i)
+    }
+}
+
+
+
