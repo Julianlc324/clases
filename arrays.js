6ea75bded17efa5982da6560b1bcd348a2d9f33a
julian <julilc324@gmail.com>
2022-03-29 22:28:49 -0500

    pendiente ejerciccios

--- arrays.js
+++ arrays.js
@@ -33,8 +33,38 @@
 var peliculas = new Array("la verdad duele", "la vida es bella", "gran torino")
 peliculas.push("batman")
 
+peliculas.forEach(elemento => {
+    console.log(elemento)
+});
+
 //var elemento = prompt("ingrese tu pelicula: ")
 //peliculas.push(elemento)
-peliculas.pop()
-console.log(peliculas)
+//peliculas.pop() //ultimo elemento
+//var indice = peliculas.indexOf("la verdad duele")
+//peliculas.splice(indice, 1)
+//var prueba = peliculas.join()
+//var cadena = "texto1, texto2, texto3"
+//var cadena_array = cadena.split(", ")
+//console.log(cadena_array)
+
+//peliculas.sort()
+//peliculas.reverse()
+//for (let clave in peliculas) {
+//   console.log(peliculas[clave])
+//}
+
+//console.log(peliculas)
+
+//peliculas.find(function(pelicula){
+    //return pelicula == "la vida es bella"
+//    peliculas.forEach(elemento => {
+//       console.log(elemento)
+//    });
+
+//})
+
+
+
+
+
 
